5359382197cce99bea8ba8454169f3ecfc93ba8c
SirDevrick <emalcoaccha@unsa.edu.pe>
2024-01-02 11:08:19 -0500

    Real repaso, codigo tryhard escrito de corrido dias antes del examen

---
+++ LinkedList/LLisTryhard.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct node {
+  int data;
+  struct node* next;
+};
+
+typedef struct node Node;
+
+void Push (struct node** headRef, int data) {
+  struct node* newNode = malloc(sizeof(struct node));
+  (*newNode).data = data;
+
+  (*newNode).next = *headRef;
+  *headRef = newNode;
+}
+
+int LenghtList (struct node* headRef) {
+  int len = 0;
+  while (headRef) {
+    len++;
+    headRef = (*headRef).next;
+  }
+
+  return len;
+}
+
+int Count (Node** headRef, int data) {
+  int count = 0;
+  while (*headRef) {
+    if (data == (**headRef).data) count++;
+    *headRef = (**headRef).next;
+  }
+  return count;
+}
+
+int GetNth (Node* headRef, int idx) {
+  assert(idx >= 0 && idx < LenghtList(headRef));
+  while (idx != 0) {
+    headRef = (*headRef).next;
+    idx--;
+  }
+  return (*headRef).data;
+}
+
+void LenghtTest () {
+  struct node* head = NULL;
+  Push(&head, 1);
+  Push(&head, 2);
+  Push(&head, 3);
+
+  int len = LenghtList(head);
+  printf("%d\n", len);
+}
+
+void DeleteListDommie (Node** headRef) {
+  if (*headRef == NULL)
+    return;
+  else {
+    DeleteListDommie(&((*headRef)->next));
+    free(*headRef);
+    *headRef = NULL;
+  }
+}
+
+void DeleteListChad (Node** headRef) {
+  Node* current = *headRef;
+  Node* aux = *headRef;
+  while (aux != NULL) {
+    aux = aux->next;
+    free(current);
+    current=aux;
+  }
+  *headRef = NULL;
+}
+
+void CountTest () {
+  struct node* head = NULL;
+  Push(&head, 2);
+  Push(&head, 2);
+  Push(&head, 2);
+
+  int len = Count(&head, 2);
+  printf("%d\n", len);
+}
+
+void GetNthTest () {
+  struct node* head = NULL;
+  Push(&head, 1);
+  Push(&head, 2);
+  Push(&head, 3);
+
+  int len = GetNth(head, 2);
+  printf("%d\n", len);
+}
+
+void DeleteTest () {
+  struct node* head = NULL;
+  Push(&head, 3);
+  Push(&head, 2);
+  Push(&head, 5);
+  DeleteListDommie(&head);
+  int len = LenghtList(head);
+  printf("%d\n", len);
+}
+
+void DeleteChadTest () {
+  struct node* head = NULL;
+  Push(&head, 3);
+  Push(&head, 2);
+  Push(&head, 5);
+  DeleteListChad(&head);
+  int len = LenghtList(head);
+  printf("%d\n", len);
+}
+
+int main () {
+  /* LenghtTest(); */
+  /* CountTest(); */
+  /* GetNthTest(); */
+  /* DeleteTest(); */
+  DeleteChadTest();
+  return 0;
+}
