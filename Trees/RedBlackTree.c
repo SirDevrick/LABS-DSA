5e88a91f429d9ee5cd0c1afd041805cbd8642f2c
SirDevrick <emalcoaccha@unsa.edu.pe>
2024-01-18 13:37:03 -0500

    se ha creado la estructura de un nodo para un RedBlackTree

--- Trees/RedBlackTree.c
+++ Trees/RedBlackTree.c
@@ -1,1 +1,12 @@
-/* #include "RedBlackTree.h" */
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int data;
+  char color;
+  struct node *parent;
+  struct node *left;
+  struct node *right;
+};
+
+int main() { return 0; }
